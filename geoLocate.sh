ed521408158a13925eaa4c8fa3eebdeef3d49732
istvan <kosztik@gmail.com>
2016-12-09 16:21:44 +0100

    Create geoLocate.sh

---
+++ geoLocate.sh
@@ -0,0 +1,18 @@
+# 
+# Usage: echo "xxx.yyy.ccc.aaa" | geoLocate.sh
+#
+
+while read line
+do
+    ipcim=$line
+    COUNTRY=`geoiplookup -f "/root/GeoIP/GeoIP.dat" $ipcim| cut -d' ' -f4| cut -d',' -f1`
+
+    
+    curl -s --data "IP=$ipcim" http://software77.net/geo-ip/ >/tmp/.kib_loc
+    CIDR=`cat /tmp/.kib_loc | grep ALTBG-0| cut -d$'\n' -f5|cut -d'>' -f2|cut -d'<' -f1`
+    CIDR1=`cat /tmp/.kib_loc | grep ALTBG-1| cut -d$'\n' -f7|cut -d'>' -f2|cut -d'<' -f1`
+    CIDR2=`cat /tmp/.kib_loc | grep ALTBG-1| cut -d$'\n' -f6|cut -d'>' -f2|cut -d'<' -f1`
+
+    echo $ipcim $CIDR $CIDR2 $CIDR1
+
+done < "${1:-/dev/stdin}"
